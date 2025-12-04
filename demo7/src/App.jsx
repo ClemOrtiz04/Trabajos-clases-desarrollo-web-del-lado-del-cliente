e6499b593a989903ba675f89e31427c2d5e6b85e
Elpapudrex <soyel@papu.drex>
2025-12-04 08:13:22 -0700

    Ejemplo usando fetch apirest

---
+++ demo7/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,latlng,maps,timezones,continents')
+    .then((res) => res.json())
+    .then((result) => setData(result))
+  }, []); //Ejecutar solo la primera vez que se crea el componente
+
+
+  return (
+    <>
+      <div className="container mt-3">
+        <h1 className='display-1'>Ejemplo de UseEffect en React</h1>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name and population</th>
+              <th scope="col">LatLng</th>
+              <th scope="col">Maps</th>
+              <th scope="col">Timezones</th>
+            </tr>
+          </thead>
+          <tbody>
+              {
+                data.map((item) => (
+                  <tr key = { item.name.common }>
+                    <td> { item.name.common } - { item.population} </td>
+                    <td> { item.name.official }</td>
+                    <td> { item.latlng }</td>
+                    <td> { item.maps.googleMaps }</td>
+                    <td> { item.timezones }</td>
+                  </tr>
+                ))
+              }
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default App
